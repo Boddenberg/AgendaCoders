914c083a09387538475223d09bafdd4bd3048a56
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2022-12-08 09:00:34 -0300

    feat: componente de input scanner nextLine nullable e não nullable

---
+++ src/main/java/coders/agenda/Utils/Input.java
@@ -0,0 +1,44 @@
+package coders.agenda.Utils;
+
+import coders.agenda.Utils.Errors.NotNullable;
+
+import java.util.Scanner;
+
+public class Input {
+    public static String string() {
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static String stringNotNullable(String nomeDoCampo, int tentativas) throws Exception {
+        int tentadas = 0;
+        boolean isInvalid;
+        String input = "";
+
+        do {
+            isInvalid = false;
+
+            try {
+                System.out.printf("Por favor informe o %s", nomeDoCampo);
+                input = new Scanner(System.in).nextLine().trim();
+
+                if (input.isBlank()) throw new NotNullable(nomeDoCampo);
+
+
+            } catch (NotNullable ex) {
+                tentadas += 1;
+
+                System.out.printf("msg: %s, code: %s. tente novamente", ex.getMessage(), ex.getCode());
+                if (tentadas < tentativas) {
+                    isInvalid = true;
+                    continue;
+                }
+                
+                throw new Exception("Número de tentativas excedidas");
+            }
+
+        } while (isInvalid);
+
+        return input;
+    }
+
+}
