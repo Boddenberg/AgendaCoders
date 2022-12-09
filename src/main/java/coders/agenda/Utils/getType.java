6333d0deb966f01f38b724af8f1d8f097a046147
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2022-12-09 13:51:22 -0300

    Tentando criar um generic de enum

--- src/main/java/coders/agenda/Utils/getType.java
+++ src/main/java/coders/agenda/Utils/getType.java
@@ -1,36 +1,32 @@
 package coders.agenda.Utils;
 
-import coders.agenda.Enums.TipoContato;
 import coders.agenda.Utils.Errors.InvalidParams;
 
-public class getType<T> {
+public class getType<T extends Enum<T>> {
 
-       T tipoObjeto ;
+    T tipoObjeto;
 
-    public T exec() throws InvalidParams {
-
-        System.out.println("Informe o tipo do contato");
-
-        for (T tipo : T.values()) {
-            System.out.printf("-> [%s] %s\n", tipo.ordinal(), tipo.name());
-        }
-
-        System.out.print("Escolha uma opção: ");
-        int typeOption = Input.integer();
-        System.out.print("\n");
-
-        if (typeOption < 0 || typeOption >= T.values().length)
-            throw new InvalidParams("tipo do contato");
-
-
-        T contactType = T.Pessoal;
-
-        for (T type : T.values()) {
-            if (typeOption == type.ordinal()) {
-                contactType = type;
-            }
-        }
-
-        return contactType;
-    }
+//    public <T extends Enum<T>> T exec(Class<T> enumType) throws InvalidParams {
+//
+//        System.out.println("Informe o tipo do contato");
+//
+//        for (T tipo : enumType.getEnumConstants()) {
+//            System.out.printf("-> [%s] %s\n", tipo.ordinal(), tipo.name());
+//        }
+//
+//        System.out.print("Escolha uma opção: ");
+//        int typeOption = Input.integer();
+//        System.out.print("\n");
+//
+//        if (typeOption < 0 || typeOption >= enumType.getEnumConstants().length)
+//            throw new InvalidParams("tipo do contato");
+//
+//        for (T type : enumType.getEnumConstants()) {
+//            if (typeOption == type.ordinal()) {
+//                tipoObjeto = type;
+//            }
+//        }
+//
+//        return tipoObjeto;
+//    }
 }
