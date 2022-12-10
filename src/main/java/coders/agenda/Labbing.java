556134d37c4e969c719e810a564d061e5ac9a9fa
Batcleta <35137108+Batcleta@users.noreply.github.com>
2022-12-09 23:56:34 -0300

    concluindo o dataService com as funções do crud
    Funções adicionadas e testadas

--- src/main/java/coders/agenda/Labbing.java
+++ src/main/java/coders/agenda/Labbing.java
@@ -1,25 +1,38 @@
 package coders.agenda;
 
-import coders.agenda.Utils.Input;
-import coders.agenda.Utils.menuCreator;
+import coders.agenda.Enums.TipoContato;
+import coders.agenda.Enums.TipoEndereco;
+import coders.agenda.Enums.TipoTelefone;
+import coders.agenda.Models.Contato;
+import coders.agenda.Models.Endereco;
+import coders.agenda.Models.Telefone;
+import coders.agenda.dataServices.AgendaDAO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Labbing {
     public static void main(String[] args) {
 
         try {
-            //testando componente menuCreator
-            Integer teste = menuCreator.exec("Por favor escolha uma opção", "voltar", "Criar contato", "editar contato", "excluir contato");
-            System.out.println(teste);
+//            List<Telefone> telefones = new ArrayList<>();
+//            List<Endereco> endereços = new ArrayList<>();
+//
+//            Telefone telefone2 = new Telefone(TipoTelefone.Celular, "55", "11", "963642358", "254", "washington Ferreira");
+//            telefones.add(telefone2);
+//
+//            Endereco endereco2 = new Endereco(TipoEndereco.Residencial, "Rua bento de barros", "200", "Bloco 1 ap 82", "Jd Amaralina", "Sâo Paulo", "SP", "05570200", "Brasil");
+//            endereços.add(endereco2);
+//
+//            Contato mock = new Contato(TipoContato.Pessoal, "Adolfo", "Pentellis", endereços, telefones);
+//            new AgendaDAO().adicionarDados(mock);
 
-            if(teste == 0){
-                System.out.println("voltaondo ao menu anterior...");
-            }
+//            List<Contato>teste = new AgendaDAO().listarDados();
+//            System.out.println(teste.get(0).getFullName());
 
-            String inputStringTeste = Input.string("nome");
-            System.out.println(inputStringTeste);
 
-            String inputNotNullableStringTeste = Input.stringNotNullable("teste de string", 5);
-            System.out.println(inputNotNullableStringTeste);
+            new AgendaDAO().excluirDados(1);
+
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
--- src/main/java/coders/agenda/dataServices/AgendaDAO.java
+++ src/main/java/coders/agenda/dataServices/AgendaDAO.java
@@ -6,7 +6,6 @@ import coders.agenda.Enums.TipoTelefone;
 import coders.agenda.Models.Contato;
 import coders.agenda.Models.Endereco;
 import coders.agenda.Models.Telefone;
-import coders.agenda.Utils.Errors.InvalidParams;
 import org.json.*;
 
 import java.io.IOException;
@@ -14,147 +13,110 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AgendaDAO {
-     JSONArray myObject = new JSONArray();
-     List<Contato> contatos = new ArrayList<>();
     private Path dbContato = Paths.get("src/main/java/coders/agenda/Database/Agenda.txt");
 
-//    public static void main(String[] args) throws IOException {
-//        criarArquivo();
-//        try {
-//            mockingContacts("Washington", "Ferreira");
-//            mockingContacts("Pedro", "Ferreira");
-//            mockingContacts("Elza", "Santos");
-//        } catch (InvalidParams e) {
-//            throw new RuntimeException(e);
-//        }
-//
-////        System.out.println(contatos.get(0).getTipoContato().toString());
-//        //https://ourcodeworld.com/articles/read/836/how-to-work-with-json-easily-in-java
-//
-//        AddToBase();
-//        readFromFile();
-//
-//
-//    }
-
-    private  void mockingContacts(String nome, String Sobrenome) throws InvalidParams {
-        List<Telefone> telefones = new ArrayList<>();
-        List<Endereco> endereços = new ArrayList<>();
-
-//        Telefone telefone1 = new Telefone(TipoTelefone.Celular, "11", "963642358", "washington Ferreira");
-        Telefone telefone2 = new Telefone(TipoTelefone.Celular, "55", "11", "963642358", "254", "washington Ferreira");
-//        telefones.add(telefone1);
-        telefones.add(telefone2);
-
-        Endereco endereco2 = new Endereco(TipoEndereco.Residencial, "Rua bento de barros", "200", "Bloco 1 ap 82", "Jd Amaralina", "Sâo Paulo", "SP", "05570200", "Brasil");
-        endereços.add(endereco2);
-
-        Contato mock = new Contato(TipoContato.Pessoal, nome, Sobrenome, endereços, telefones);
-        contatos.add(mock);
-    }
-
-    public  void AddToBase(Contato contatos) throws IOException {
-        List<String> contatosString = new ArrayList<>();
-            JSONObject myObject = new JSONObject();
-
-        myObject.put("nome", contatos.getNome());
-        myObject.put("sobrenome", contatos.getSobreNome());
-        myObject.put("tipoContato", contatos.getTipoContato());
-
-        myObject.put("enderecos", contatos.getEnderecos());
-        myObject.put("telefones", contatos.getTelefones());
-
-        contatosString.add(myObject.toString());
-
-        Files.write(dbContato, contatosString);
+    public void adicionarDados(Contato contato) throws IOException {
+        criarArquivo();
+        JSONObject data = new JSONObject();
+        Files.writeString(dbContato, convertToJson(contato) + "\n", StandardOpenOption.APPEND);
         System.out.print("Dados gravados com sucesso\n\n");
     }
 
-    public   List<Contato> pegarDados() throws IOException {
+    private List<String> getDataList() throws IOException {
+        List<String> listarContatos = Files.readAllLines(dbContato, StandardCharsets.UTF_8);
+        return listarContatos;
+    }
+
+    public List<Contato> listarDados() throws IOException {
         List<Contato> contatosString = new ArrayList<>();
+        List<String> data = getDataList();
 
-        try {
-            List<String> data = Files.readAllLines(dbContato, StandardCharsets.UTF_8);
+        for (int i = 0; i < data.size(); i++) {
 
-            for (int i = 0; i < data.size(); i++) {
+            List<Endereco> enderecos = new ArrayList<>();
+            List<Telefone> telefones = new ArrayList<>();
 
-                List<Endereco> enderecos = new ArrayList<>();
-                List<Telefone> telefones = new ArrayList<>();
+            JSONObject contato = new JSONObject(data.get(i));
 
-                JSONObject contato = new JSONObject(data.get(i));
+            JSONArray getEnderecos = new JSONArray(contato.get("enderecos").toString());
+            for (int j = 0; j < getEnderecos.length(); j++) {
+                JSONObject endereco = new JSONObject(getEnderecos.get(j).toString());
 
-                JSONArray getEnderecos = new JSONArray(contato.get("enderecos").toString());
-                for (int j = 0; j < getEnderecos.length(); j++) {
-                    JSONObject endereco = new JSONObject(getEnderecos.get(j).toString());
+                Endereco novoEndereco = new Endereco(
+                        TipoEndereco.Residencial,
+                        endereco.get("logradouro").toString(),
+                        endereco.get("numero").toString(),
+                        endereco.get("complemento").toString(),
+                        endereco.get("bairro").toString(),
+                        endereco.get("cidade").toString(),
+                        endereco.get("uf").toString(),
+                        endereco.get("cep").toString(),
+                        endereco.get("pais").toString()
+                );
 
-                    Endereco novoEndereco = new Endereco(
-                            TipoEndereco.Residencial,
-                            endereco.get("logradouro").toString(),
-                            endereco.get("numero").toString(),
-                            endereco.get("complemento").toString(),
-                            endereco.get("nairro").toString(),
-                            endereco.get("cidade").toString(),
-                            endereco.get("uf").toString(),
-                            endereco.get("cep").toString(),
-                            endereco.get("pais").toString()
-                    );
-
-                    enderecos.add(novoEndereco);
-
-                    System.out.println(endereco.names());
-
-                }
-
-                JSONArray myJson = new JSONArray(contato.get("telefones").toString());
-                for (int k = 0; k < myJson.length(); k++) {
-                    JSONObject endereco = new JSONObject(myJson.get(k).toString());
-                    // TipoTelefone tipoTelefone, String ddi, String ddd, String numero, String ramal, String contato
-                    Telefone novoTelefone = new Telefone (
-                            TipoTelefone.Celular,
-                            endereco.get("ddi").toString(),
-                            endereco.get("ddd").toString(),
-                            endereco.get("numero").toString(),
-                            endereco.get("ramal").toString(),
-                            endereco.get("contato").toString()
-                    );
-
-                    telefones.add(novoTelefone);
-
-                    System.out.println(endereco.names());
-
-                }
-
-                Contato contatoResponse = new Contato(TipoContato.Pessoal, contato.get("nome").toString(), contato.get("sobrenome").toString());
-                contatosString.add(contatoResponse);
-
-                System.out.println("");
+                enderecos.add(novoEndereco);
             }
 
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            JSONArray myJson = new JSONArray(contato.get("telefones").toString());
+            for (int k = 0; k < myJson.length(); k++) {
+                JSONObject endereco = new JSONObject(myJson.get(k).toString());
+                // TipoTelefone tipoTelefone, String ddi, String ddd, String numero, String ramal, String contato
+                Telefone novoTelefone = new Telefone(
+                        TipoTelefone.Celular,
+                        endereco.get("ddi").toString(),
+                        endereco.get("ddd").toString(),
+                        endereco.get("numero").toString(),
+                        endereco.get("ramal").toString(),
+                        endereco.get("contato").toString()
+                );
+
+                telefones.add(novoTelefone);
+            }
+
+            Contato contatoResponse = new Contato(TipoContato.Pessoal, contato.get("nome").toString(), contato.get("sobrenome").toString());
+            contatosString.add(contatoResponse);
+
+            System.out.println("");
         }
 
         return contatosString;
-
     }
 
-    public void excluirContato(Contato contato) throws IOException {
-        List<Contato> contatos = pegarDados();
-
-        //pegar o contato na lista
-
-//        Files.write(contatos);
+    public void editarDados(Contato contato, int posicao) throws IOException {
+        List<String> listarOriginais = getDataList();
+        listarOriginais.set(posicao, convertToJson(contato)); // passar contato para json
+        Files.write(dbContato, listarOriginais);
     }
 
-    //Metodo de excluir todos os contatos
-    
-    
+    private String convertToJson(Contato contato) {
+        JSONObject data = new JSONObject();
+        data.put("nome", contato.getNome());
+        data.put("sobrenome", contato.getSobreNome());
+        data.put("tipoContato", contato.getTipoContato());
 
-    private  void criarArquivo() {
+        data.put("enderecos", contato.getEnderecos());
+        data.put("telefones", contato.getTelefones());
+
+        return data.toString();
+    }
+
+    public void excluirDados(int posicao) throws IOException {
+        List<String> listarOriginais = getDataList();
+        listarOriginais.remove(posicao);
+        Files.write(dbContato, listarOriginais);
+    }
+
+    public void limparAgenda() throws IOException {
+        Files.writeString(dbContato, "");
+    }
+
+
+    private void criarArquivo() {
 
         try {
             if (Files.exists(dbContato)) {
