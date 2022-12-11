4e66db8b8a1fd4be980c376d6ace4beb615a9c7e
Batcleta <35137108+Batcleta@users.noreply.github.com>
2022-12-11 11:41:55 -0300

    test: testando funções de AgendaDAO - sucesso

--- src/main/java/coders/agenda/Labbing.java
+++ src/main/java/coders/agenda/Labbing.java
@@ -15,17 +15,23 @@ public class Labbing {
     public static void main(String[] args) {
 
         try {
-//            List<Telefone> telefones = new ArrayList<>();
-//            List<Endereco> endereços = new ArrayList<>();
-//
-//            Telefone telefone2 = new Telefone(TipoTelefone.Celular, "55", "11", "963642358", "254", "washington Ferreira");
-//            telefones.add(telefone2);
-//
-//            Endereco endereco2 = new Endereco(TipoEndereco.Residencial, "Rua bento de barros", "200", "Bloco 1 ap 82", "Jd Amaralina", "Sâo Paulo", "SP", "05570200", "Brasil");
-//            endereços.add(endereco2);
-//
-//            Contato mock = new Contato(TipoContato.Pessoal, "Adolfo", "Pentellis", endereços, telefones);
-//            new AgendaDAO().adicionarDados(mock);
+            List<Telefone> telefones = new ArrayList<>();
+            List<Endereco> endereços = new ArrayList<>();
+
+            Telefone telefone1 = new Telefone(TipoTelefone.Residencial, "55", "11", "963642358", "254", "washington Ferreira");
+            telefones.add(telefone1);
+
+            Telefone telefone2 = new Telefone(TipoTelefone.Celular, "55", "11", "963642358", "254", "washington Ferreira");
+            telefones.add(telefone2);
+
+            Endereco endereco1 = new Endereco(TipoEndereco.Residencial, "Rua bento de barros", "200", "Bloco 1 ap 82", "Jd Amaralina", "Sâo Paulo", "SP", "05570200", "Brasil");
+            endereços.add(endereco1);
+
+            Endereco endereco2 = new Endereco(TipoEndereco.Comercial, "Rua bento de barros", "200", "Bloco 1 ap 82", "Jd Amaralina", "Sâo Paulo", "SP", "05570200", "Brasil");
+            endereços.add(endereco2);
+
+            Contato mock = new Contato(TipoContato.Profissional, "Mirna", "Pentellis", endereços, telefones);
+            new AgendaDAO().adicionarDados(mock);
 
             List<Contato>teste = new AgendaDAO().listarDados();
             System.out.println(teste.get(0).getFullName());
