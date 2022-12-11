1ff6bd2c462d33ad67f893343746773bb1574b6c
Batcleta <35137108+Batcleta@users.noreply.github.com>
2022-12-11 12:18:49 -0300

    refactor: dividindo a responsabilidade dos métodos

--- src/main/java/coders/agenda/dataServices/AgendaDAO.java
+++ src/main/java/coders/agenda/dataServices/AgendaDAO.java
@@ -20,6 +20,9 @@ import java.util.List;
 public class AgendaDAO {
     private Path dbContato = Paths.get("src/main/java/coders/agenda/Database/Agenda.txt");
 
+
+    // ========== MAJOR FUNCIONS ============
+
     public void adicionarDados(Contato contato) throws IOException {
         criarArquivo();
         JSONObject data = new JSONObject();
@@ -27,111 +30,21 @@ public class AgendaDAO {
         System.out.print("Dados gravados com sucesso\n\n");
     }
 
-    private List<String> getDataList() throws IOException {
-        List<String> listarContatos = Files.readAllLines(dbContato, StandardCharsets.UTF_8);
-        return listarContatos;
-    }
-
     public List<Contato> listarDados() throws IOException {
-        List<Contato> contatosString = new ArrayList<>();
+        List<Contato> contatos = new ArrayList<>();
         List<String> data = getDataList();
 
         for (int i = 0; i < data.size(); i++) {
-
-            List<Endereco> enderecos = new ArrayList<>();
-            List<Telefone> telefones = new ArrayList<>();
-
-            JSONObject contato = new JSONObject(data.get(i));
-            if(contato.get("tipoContato").toString().isBlank()) throw new IOException(" Erro na listagem de dados. tipoContato está nulo na base");
-
-            JSONArray listaEnderecos = new JSONArray(contato.get("enderecos").toString());
-            for (int j = 0; j < listaEnderecos.length(); j++) {
-                JSONObject endereco = new JSONObject(listaEnderecos.get(j).toString());
-
-                if(endereco.get("tipoEndereco").toString().isBlank()) throw new IOException(" Erro na listagem de dados. tipoEndereco está nulo na base");
-
-                TipoEndereco tipoEndereco = null;
-
-                for (TipoEndereco tipo : TipoEndereco.values()) {
-                    if (endereco.get("tipoEndereco").toString().equals(tipo.name())) {
-                        tipoEndereco = tipo;
-                    }
-                }
-
-                Endereco novoEndereco = new Endereco(
-                        tipoEndereco,
-                        endereco.get("logradouro").toString(),
-                        endereco.get("numero").toString(),
-                        endereco.get("complemento").toString(),
-                        endereco.get("bairro").toString(),
-                        endereco.get("cidade").toString(),
-                        endereco.get("uf").toString(),
-                        endereco.get("cep").toString(),
-                        endereco.get("pais").toString()
-                );
-
-                enderecos.add(novoEndereco);
-            }
-
-            JSONArray listaTelefones = new JSONArray(contato.get("telefones").toString());
-            for (int k = 0; k < listaTelefones.length(); k++) {
-                JSONObject telefone = new JSONObject(listaTelefones.get(k).toString());
-
-                if(telefone.get("tipoTelefone").toString().isBlank()) throw new IOException(" Erro na listagem de dados. tipoTelefone está nulo na base");
-
-                TipoTelefone tipoTelefone = null;
-
-                for (TipoTelefone tipo : TipoTelefone.values()) {
-                    if (telefone.get("tipoTelefone").toString().equals(tipo.name())) {
-                        tipoTelefone = tipo;
-                    }
-                }
-
-                Telefone novoTelefone = new Telefone(
-                        tipoTelefone,
-                        telefone.get("ddi").toString(),
-                        telefone.get("ddd").toString(),
-                        telefone.get("numero").toString(),
-                        telefone.get("ramal").toString(),
-                        telefone.get("contato").toString()
-                );
-
-                telefones.add(novoTelefone);
-            }
-
-            TipoContato tipoContato = null;
-
-            for (TipoContato tipo : TipoContato.values()) {
-                if (contato.get("tipoContato").toString().equals(tipo.name())) {
-                    tipoContato = tipo;
-                }
-            }
-
-            Contato contatoResponse = new Contato(tipoContato, contato.get("nome").toString(), contato.get("sobrenome").toString(), enderecos, telefones);
-            contatosString.add(contatoResponse);
-
-            System.out.println("");
+            ListarContatos(new JSONObject(data.get(i)), contatos);
         }
 
-        return contatosString;
+        return contatos;
     }
 
     public void editarDados(Contato contato, int posicao) throws IOException {
         List<String> listarOriginais = getDataList();
         listarOriginais.set(posicao, convertToJson(contato)); // passar contato para json
         Files.write(dbContato, listarOriginais);
-    }
-
-    private String convertToJson(Contato contato) {
-        JSONObject data = new JSONObject();
-        data.put("nome", contato.getNome());
-        data.put("sobrenome", contato.getSobreNome());
-        data.put("tipoContato", contato.getTipoContato());
-
-        data.put("enderecos", contato.getEnderecos());
-        data.put("telefones", contato.getTelefones());
-
-        return data.toString();
     }
 
     public void excluirDados(int posicao) throws IOException {
@@ -144,6 +57,8 @@ public class AgendaDAO {
         Files.writeString(dbContato, "");
     }
 
+
+    // ========== HELPER FUNCTIONS ============
 
     private void criarArquivo() {
 
@@ -158,6 +73,118 @@ public class AgendaDAO {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private void ListarContatos(JSONObject contato, List<Contato> contatos) throws IOException {
+        List<Endereco> enderecos = new ArrayList<>();
+        List<Telefone> telefones = new ArrayList<>();
+
+        if (contato.get("tipoContato").toString().isBlank())
+            throw new IOException(" Erro na listagem de dados. tipoContato está nulo na base");
+
+        if (contato.get("nome").toString().isBlank() || contato.get("sobrenome").toString().isBlank())
+            throw new IOException(" Erro na listagem de dados. nome ou sobrenome estão nulos na base");
+
+        ListarEnderecos(contato, enderecos);
+        ListarTelefones(contato, telefones);
+
+        TipoContato tipoContato = null;
+
+        for (TipoContato tipo : TipoContato.values()) {
+            if (contato.get("tipoContato").toString().equals(tipo.name())) {
+                tipoContato = tipo;
+            }
+        }
+
+        Contato contatoResponse = new Contato(tipoContato, contato.get("nome").toString(), contato.get("sobrenome").toString(), enderecos, telefones);
+        contatos.add(contatoResponse);
+    }
+
+    private void ListarEnderecos(JSONObject contato, List<Endereco> enderecos) throws IOException {
+        JSONArray listaEnderecos = new JSONArray(contato.get("enderecos").toString());
+
+        for (int j = 0; j < listaEnderecos.length(); j++) {
+            JSONObject endereco = new JSONObject(listaEnderecos.get(j).toString());
+
+            if (endereco.get("tipoEndereco").toString().isBlank())
+                throw new IOException(" Erro na listagem de dados. tipoEndereco está nulo na base");
+
+            if (endereco.get("logradouro").toString().isBlank() || endereco.get("numero").toString().isBlank())
+                throw new IOException(" Erro na listagem de dados. logradouro ou numero estão nulos na base");
+
+            TipoEndereco tipoEndereco = null;
+
+            for (TipoEndereco tipo : TipoEndereco.values()) {
+                if (endereco.get("tipoEndereco").toString().equals(tipo.name())) {
+                    tipoEndereco = tipo;
+                }
+            }
+
+            Endereco novoEndereco = new Endereco(
+                    tipoEndereco,
+                    endereco.get("logradouro").toString(),
+                    endereco.get("numero").toString(),
+                    endereco.get("complemento").toString(),
+                    endereco.get("bairro").toString(),
+                    endereco.get("cidade").toString(),
+                    endereco.get("uf").toString(),
+                    endereco.get("cep").toString(),
+                    endereco.get("pais").toString()
+            );
+
+            enderecos.add(novoEndereco);
+        }
+    }
+
+    private void ListarTelefones(JSONObject contato, List<Telefone> telefones) throws IOException {
+        JSONArray listaTelefones = new JSONArray(contato.get("telefones").toString());
+
+        for (int k = 0; k < listaTelefones.length(); k++) {
+            JSONObject telefone = new JSONObject(listaTelefones.get(k).toString());
+
+            if (telefone.get("tipoTelefone").toString().isBlank())
+                throw new IOException(" Erro na listagem de dados. tipoTelefone está nulo na base");
+
+            if (telefone.get("ddd").toString().isBlank() || telefone.get("numero").toString().isBlank() || telefone.get("contato").toString().isBlank())
+                throw new IOException(" Erro na listagem de dados. ddd, numero ou contato estão nulos na base");
+
+            TipoTelefone tipoTelefone = null;
+
+            for (TipoTelefone tipo : TipoTelefone.values()) {
+                if (telefone.get("tipoTelefone").toString().equals(tipo.name())) {
+                    tipoTelefone = tipo;
+                }
+            }
+
+            Telefone novoTelefone = new Telefone(
+                    tipoTelefone,
+                    telefone.get("ddi").toString(),
+                    telefone.get("ddd").toString(),
+                    telefone.get("numero").toString(),
+                    telefone.get("ramal").toString(),
+                    telefone.get("contato").toString()
+            );
+
+            telefones.add(novoTelefone);
+        }
+
+    }
+
+    private List<String> getDataList() throws IOException {
+        List<String> listarContatos = Files.readAllLines(dbContato, StandardCharsets.UTF_8);
+        return listarContatos;
+    }
+
+    private String convertToJson(Contato contato) {
+        JSONObject data = new JSONObject();
+        data.put("nome", contato.getNome());
+        data.put("sobrenome", contato.getSobreNome());
+        data.put("tipoContato", contato.getTipoContato());
+
+        data.put("enderecos", contato.getEnderecos());
+        data.put("telefones", contato.getTelefones());
+
+        return data.toString();
     }
 
 
